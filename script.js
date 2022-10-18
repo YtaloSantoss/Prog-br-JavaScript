5ccf1f3c07176bfb4c14ff99f5bc3f741061ed7a
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-10-18 20:04:36 -0300

    s
    atualizado

---
+++ script.js
@@ -0,0 +1,15 @@
+var idade = 19;
+
+var maior20 = idade > 20;
+var menor30 = idade < 30;
+
+var entre = maior20 || menor30;
+
+console.log("Idade", idade)
+
+console.log("Maior que vente",maior20);
+
+console.log("Menor que 30", menor30);
+
+console.log("Entre 20 e 30", entre);
+
