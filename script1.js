f66f78135351fe1764e953dd2628a338e4d7c9f8
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-10-18 21:27:58 -0300

    atualização

--- script1.js
+++ script1.js
@@ -1,3 +1,15 @@
+var idade = 17;
 
-var nome = prompt("Qual o seu nome?")
-alert("Bom dia, " + nome)
+if (idade >= 25) {
+    console.log("pode");
+    console.log("qual o seu pedido");
+}
+
+else if(idade >=18){
+    console.log("pode");
+    console.log("mostre a identidade")
+}
+
+else {
+    console.log("não pode");
+}
