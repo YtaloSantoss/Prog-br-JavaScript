dfaf6b692e989d219c0fb83a7f2233dfaaca237f
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-10-21 16:36:27 -0300

    atualizanod

---
+++ Basico/script10.js
@@ -0,0 +1,18 @@
+var calcMedia = function(){
+    return (this.nota1 + this.nota2)/2;
+}
+
+var turma = [
+            {nome:"Igor",
+            nota1:9,
+            nota2:7,
+            media:calcMedia},
+            {nome:"João",
+            nota1:4,
+            nota2:3,
+            media:calcMedia}
+        ]
+
+var aluno = turma[1]
+console.log(aluno);
+console.log(aluno.media());
